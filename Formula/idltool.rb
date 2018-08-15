c49cdd80514b2ff6a4e6022100a2592f40e6f3b3
Stein Eldar Johnsen <steineldar@zedge.net>
2018-08-15 11:50:53 +0200

    Bad sha256 sum in idltool

--- Formula/idltool.rb
+++ Formula/idltool.rb
@@ -3,7 +3,7 @@ class Idltool < Formula
     homepage "http://www.morimekta.net"
     version "2.0"
     url "https://github.com/morimekta/idltool/releases/download/v#{version}/idltool-#{version}.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "bb4d8aa993f5687f83cca7969371916add324a1afbe8bf2995d08ab541bcae77"
 
     depends_on :java => "1.8+"
 
