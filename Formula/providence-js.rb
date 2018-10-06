e4223e8c8fa9702324b2412e15fa0b5cd9ca6116
Stein Eldar Johnsen <oss@morimekta.net>
2018-10-06 20:58:31 +0200

    Add providence-js.

---
+++ Formula/providence-js.rb
@@ -0,0 +1,14 @@
+class ProvidenceJs < Formula
+    desc "Providence JS Generator"
+    homepage "https://www.morimekta.net/providence"
+    version "0.1.0"
+    url "https://bintray.com/morimekta/archive/download_file?file_path=providence-js-#{version}.tar.gz"
+    sha256 "a1bbfdb7eec9a3acbef0ddfb225202881f0a259881ceda191cb05f70d568a23a"
+
+    depends_on :java => "1.8+", :providence => "1.0.0+"
+
+    def install
+        bin.install Dir["bin/*"]
+        share.install Dir["share/*"]
+    end
+end
