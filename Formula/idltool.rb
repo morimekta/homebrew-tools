2f29fb25e7f409eaec932abbe3684ba8d826bfad
Stein Eldar Johnsen <oss@morimekta.net>
2019-02-05 17:59:00 +0100

    Fixed idltool for detatched head.

--- Formula/idltool.rb
+++ Formula/idltool.rb
@@ -1,9 +1,9 @@
 class Idltool < Formula
     desc "IDL Tool"
     homepage "https://www.morimekta.net"
-    version "3.0"
+    version "3.1"
     url "https://bintray.com/morimekta/archive/download_file?file_path=idltool-#{version}.tar.gz"
-    sha256 "5941e546065ee52c0ac1d92814f6429bf2f83e6a91ad96c092d0bf1b5ab918a4"
+    sha256 "af22143600c796fecc87dc0f05a359a28c836e70199d8fe5f8bfe4362de05046"
 
     depends_on :java => "1.8+"
 
