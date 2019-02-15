6ff6ffc60528dad7727243c2c5b6bfa9be7df50a
Stein Eldar Johnsen <oss@morimekta.net>
2019-02-15 23:00:33 +0100

    dltool 3.1.1.

--- Formula/idltool.rb
+++ Formula/idltool.rb
@@ -1,9 +1,9 @@
 class Idltool < Formula
     desc "IDL Tool"
     homepage "https://www.morimekta.net"
-    version "3.1"
+    version "3.1.1"
     url "https://bintray.com/morimekta/archive/download_file?file_path=idltool-#{version}.tar.gz"
-    sha256 "af22143600c796fecc87dc0f05a359a28c836e70199d8fe5f8bfe4362de05046"
+    sha256 "9a52ebeef419b826743946743964b211ffdba7d28430cdb84f308065f5cb0e51"
 
     depends_on :java => "1.8+"
 
