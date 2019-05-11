b73becc8a55e00ae548a058f6a610c3cb630e504
Stein Eldar Johnsen <oss@morimekta.net>
2019-05-11 14:36:39 +0200

    Providence 2.0-beta1

--- Formula/providence.rb
+++ Formula/providence.rb
@@ -1,9 +1,9 @@
 class Providence < Formula
     desc "Providence Tools"
     homepage "http://www.morimekta.net/providence"
-    version "1.10.0"
-    url "https://bintray.com/morimekta/archive/download_file?file_path=providence-tools-#{version}.tar.gz"
-    sha256 "d006d116b7b6f23c49ebe61477da90aa89f26987fec98e2a608b3393ecf31f76"
+    version "2.0-beta1"
+    url "https://dl.morimekta.net/archive/providence-#{version}.tar.gz"
+    sha256 "156b1d9e06a03727c6887cf00dc84b0c1132669782c37573b518bec2c04cd494"
 
     depends_on :java => "1.8+"
 
